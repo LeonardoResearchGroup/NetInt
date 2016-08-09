2ee9f8a0b5e40dbb09c9153bf3fcfc025b4aa19e
lfrivera <lfrivera@icesi.edu.co>
2016-08-09 00:27:57 -0500

    rJava test implementation added. Test process must be completed.

--- Java/JavaR-PoC/src/main/Executable.java
+++ Java/JavaR-PoC/src/main/Executable.java
@@ -1,5 +1,27 @@
 package main;
 
+import poc.rjava.RJavaHelper;
+
 public class Executable {
 
+	public static void main(String[] args)
+	{
+		testrJava();
+		//testrServe();
+	}
+	
+	private static void testrJava()
+	{
+		//http://www.codophile.com/how-to-integrate-r-with-java-using-rjava/
+		
+		RJavaHelper.getInstance().assignVariable("vector", "c(1,2,3,4,5)");
+		RJavaHelper.getInstance().assignVariableFromFunction("mean", "mean", "vector");
+		double mean = RJavaHelper.getInstance().obtainValueFromVariableAsDouble("mean");
+		System.out.println("Mean: "+mean);
+	}
+	
+	private static void testrServe()
+	{
+		//http://www.codophile.com/how-to-integrate-r-with-java-using-rserve/
+	}
 }
---
+++ Java/JavaR-PoC/src/poc/rserve/RServeHelper.java
@@ -0,0 +1,5 @@
+package poc.rserve;
+
+public class RServeHelper {
+
+}
