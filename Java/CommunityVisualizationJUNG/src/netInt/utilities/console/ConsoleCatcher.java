102874d8cef2f203d04e5075e161fb63b31f76a7
Juan Salamanca <jsalam@icesi.edu.co>
2017-11-16 17:11:12 -0500

    New Class Header including copyrights for Bancolombia

--- Java/CommunityVisualizationJUNG/src/netInt/utilities/console/ConsoleCatcher.java
+++ Java/CommunityVisualizationJUNG/src/netInt/utilities/console/ConsoleCatcher.java
@@ -1,14 +1,20 @@
 /*******************************************************************************
- * This library is free software. You can redistribute it and/or modify it under
- * the terms of the GNU Lesser General Public License as published by the 
- * Free Software Foundation; either version 2.1 of the License, or (at your option) 
- * any later version. This library is distributed  WITHOUT ANY WARRANTY;
- * without even the implied warranty of  MERCHANTABILITY or FITNESS FOR A 
- * PARTICULAR PURPOSE. See the file COPYING included with this distribution 
- * for more information.
- * 	
- * Copyright (c) 2017 Universidad Icesi. All rights reserved. www.icesi.edu.co
- *******************************************************************************/
+ * Copyright (C) 2017 Universidad Icesi & Bancolombia
+ *  
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *  
+ * You should have received a copy of the GNU Lesser General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+
 package netInt.utilities.console;
 
 import java.io.ByteArrayOutputStream;
