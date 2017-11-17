54d6abf838750488e993d7f136ce6d528555f7e1
Juan Salamanca <jsalam@icesi.edu.co>
2017-11-16 20:03:22 -0500

    Fine tuning Class Header including copyrights for Bancolombia

--- Java/CommunityVisualizationJUNG/src/netInt/utilities/geometry/Involute.java
+++ Java/CommunityVisualizationJUNG/src/netInt/utilities/geometry/Involute.java
@@ -1,6 +1,4 @@
 /*******************************************************************************
- * Copyright (C) 2017 Universidad Icesi & Bancolombia
- *  
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -12,9 +10,10 @@
  * GNU Lesser General Public License for more details.
  *  
  * You should have received a copy of the GNU Lesser General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Copyright (C) 2017 Universidad Icesi & Bancolombia
  ******************************************************************************/
- 
 package netInt.utilities.geometry;
 
 import processing.core.PApplet;
